9c6deb73cd0972b461d75c356def45edf14a624d
Panych <panych.net@gmail.com>
2025-10-10 16:21:56 +0200

    Development

--- parts/news_teaser2.php
+++ parts/news_teaser2.php
@@ -3,11 +3,12 @@ $thumbnail_image = get_field('thumbnail_image');
 $category = get_field('category');
 if ( empty( $thumbnail_image ) ) {
     $thumbnail_image = get_field('image', 'term_'.$category->term_id);
+    $no_overlay = true;
 }
 ?>
 <div class="newsTeaser2">
     <div class="imageBlock imageBlock--formatted newsTeaser2__imageBlock"><?php echo wp_get_attachment_image( $thumbnail_image['ID'], 'full', false, ['class'=>'newsTeaser2__image'] ); ?></div>
-    <a class="newsTeaser2__content" href="<?php echo get_the_permalink(); ?>">
+    <a class="newsTeaser2__content <?php if ( !empty($no_overlay) ) : ?>noOverlay<?php endif; ?>" href="<?php echo get_the_permalink(); ?>">
         <span class="newsTeaser2__date"><?php echo $category->name; ?></span>
         <h3 class="newsTeaser2__title"><?php echo get_the_title(); ?></h3>
         <span class="newsTeaser2__button"></span>
